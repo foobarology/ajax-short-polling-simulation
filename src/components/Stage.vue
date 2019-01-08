0fa437b3a2594e0ab39cfb6bda688e489ce4d73a
Muhammad Syifa <emsifa@gmail.com>
2019-01-08 16:40:43 +0700

    Improve no buttons mode

--- src/components/Stage.vue
+++ src/components/Stage.vue
@@ -91,13 +91,15 @@ export default {
   },
   mounted () {
     if (!this.buttons) {
-      setTimeout(() => {
-        this.client1StartPolling();
-      }, 1000);
-
       window.onkeydown = e => {
         if (e.ctrlKey && e.keyCode == 13) {
-          this.client2PostMessage();
+          if (!this.polling) {
+            this.client1StartPolling();
+          } else {
+            if (!this.client2HttpActive) {
+              this.client2PostMessage();
+            }
+          }
         }
       };
     }
